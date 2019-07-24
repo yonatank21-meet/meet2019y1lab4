9b4127c6c0c56727095a561ec3f989d466ecd5d2
yonatank21-meet <yonatank21@meet.mit.edu>
2019-07-24 14:27:36 +0300

    Stopping here

---
+++ funturtle.py
@@ -0,0 +1,23 @@
+import turtle
+turtle.shape('turtle')
+finn = turtle.clone()
+finn.shape('square')
+finn.goto(100,100)
+finn.forward(100)
+finn.left(90)
+finn.forward(100)
+finn.left(90)
+finn.forward(100)
+finn.left(90)
+finn.forward(100)
+charlie = turtle.clone()
+charlie.shape('triangle')
+charlie.forward(100)
+charlie.left(120)
+charlie.forward(100)
+charlie.left(120)
+charlie.forward(100)
+
+
+
+turtle.mainloop()
