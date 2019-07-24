d8cc7bc9c97424989270032548d1be134295c666
yonatank21-meet <yonatank21@meet.mit.edu>
2019-07-24 17:02:30 +0300

    Stopping here

---
+++ fruit_sorter.py
@@ -0,0 +1,9 @@
+fruit = input("Hi there! what fruit would you like me to sort today?")
+if fruit == "Apple":
+    print("Apples go to bin 1!")
+elif fruit == "Orange":
+          print("Oranges go in bin 2!")
+elif fruit == "Banana":
+          print("Bananas go in bin 3!")
+else:
+    print("sorry i can only sort  bananas oranges or apples...")
